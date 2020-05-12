ce98460599f39a5ff955ea5cd01e515158c5970c
Shane Passon <scpasson@yahoo.com>
2020-05-11 18:06:20 -0700

    Introduced Routing

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
+import './index.css';
+import App from './App';
+
+
+ReactDOM.render(
+  <BrowserRouter>
+<App />  
+  </BrowserRouter>
+  //<React.StrictMode>
+  //</React.StrictMode>,
+  ,document.getElementById('root')
+);
+
